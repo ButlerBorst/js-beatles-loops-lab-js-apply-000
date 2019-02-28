0a84e85c5073df543b23fbd7e2f949fcb8108cee
Alex Borst <butler.borst@gmail.com>
2019-02-28 17:45:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,23 +1,12 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
-  var array = [];
-  for (var i = 0; i < musicians.length; i++) {
-    array.push(musicians[i] + " plays " + instruments[i]);
-  }
-  return array;
-}
-
-function theBeatlesPlay(musicians, instruments) {
   var blank = [];
   for (var i=0; i < musicians.length; i++) {
-}
-
-function theBeatlesPlay(musicians, instruments) {
-  var array = [];
-  for (var i = 0; i < musicians.length; i++) {
-  	array.push(musicians[i] + ' plays ' + instruments[i]);
+    array.push(musicians[i] + " plays " + instruments{i});
   }
-  return array;
+  return blank;
+
+
 
  function johnLennonFacts() {
     var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
