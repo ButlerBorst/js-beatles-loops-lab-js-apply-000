1a709534058289a27cdd5cafa0475a99a2f66c9d
Alex Borst <butler.borst@gmail.com>
2019-02-28 17:48:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,26 +5,7 @@ function theBeatlesPlay(musicians, instruments) {
     array.push(musicians[i] + " plays " + instruments[i]);
   }
   return blank;
-
-
-
- function johnLennonFacts() {
-    var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
-    var newFacts = [];
-    var i = 0;
-    while (i < facts.length) {
-        newFacts.push(facts[i] +"!!!");
-        i++;
-    }
-    return newFacts;
 }
 
- function iLoveTheBeatles(num) {
-    var love = [];
-    do {
-        love.push("I love the Beatles!");
-        num++;
-    } while (num < 15);
 
-     return love;
-}
+
