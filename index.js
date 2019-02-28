5518f9d6857741614328e2484259b8947a5bc30b
Alex Borst <butler.borst@gmail.com>
2019-02-28 17:40:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
 // add solution here
-function theBeatlesPlay(players, instruments) {
-    var allPlayers = [];
-    for (var i=0; i < players.length; i++) {
-        allPlayers.push(players[i] + " plays " + instruments[i]);
-    }
-    return allPlayers;
-}
-
-
 function theBeatlesPlay(musicians, instruments) {
   var array = [];
   for (var i = 0; i < musicians.length; i++) {
     array.push(musicians[i] + " plays " + instruments[i]);
   }
   return array;
+}
+
+function theBeatlesPlay(musicians, instruments) {
+  var blank = [];
+  for (var i=0; i < musicians.length; i++) {
 }
 
 function theBeatlesPlay(musicians, instruments) {
