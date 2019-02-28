45c95ba861a2468822efb0725039f1121c92729d
Alex Borst <butler.borst@gmail.com>
2019-02-28 17:32:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ function theBeatlesPlay(players, instruments) {
     return allPlayers;
 }
 
+
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (var i = 0; i < musicians.length; i++) {
+    array.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return array;
+}
+
  function johnLennonFacts() {
     var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
     var newFacts = [];
